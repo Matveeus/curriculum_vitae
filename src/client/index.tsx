62222ed7a60f218482e24903331b9673e92a08ad
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-07-21 16:38:09 +0300

    add authentication slice

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
+import { Provider as StoreProvider } from 'react-redux';
 import { client } from '../apollo/client';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <BrowserRouter>
     <React.StrictMode>
       <ApolloProvider client={client}>
-        <App />
+        <StoreProvider store={store}>
+          <App />
+        </StoreProvider>
       </ApolloProvider>
     </React.StrictMode>
   </BrowserRouter>,
