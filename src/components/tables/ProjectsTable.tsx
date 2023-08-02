176ab66472bd27f7d46009f7325afd2a2c589088
Matvey Lapitchev <matvey.lapitchev@gmail.com>
2023-08-02 13:01:59 +0200

    add projects table

---
+++ src/components/tables/ProjectsTable.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { GET_PROJECTS } from '../../apollo/operations/projects';
+import { useQuery } from '@apollo/client';
+import InitialTable from './InitialTable';
+import { Loader } from '../';
+import routes from '../../constants/routes';
+import { useNavigate } from 'react-router-dom';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import type { Project } from '../../apollo/types';
+import type { MenuItemData } from '../MoreMenu';
+import type { Column } from './InitialTable';
+
+interface Data {
+  id: string;
+  name: string;
+  internalName: string;
+  startDate: string;
+  endDate: string;
+  domain: string;
+  teamSize: number;
+  menuItems?: MenuItemData[];
+}
+
+export default function ProjectsTable() {
+  const navigate = useNavigate();
+  const currentUser = useTypedSelector(state => state.auth.currentUser);
+  const { loading, error, data } = useQuery<{ projects: Project[] }>(GET_PROJECTS);
+
+  if (loading) return <Loader />;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const projects = data?.projects || [];
+
+  const columns: Column[] = [
+    { id: 'name', label: 'Name', align: 'center', searchable: true },
+    { id: 'internalName', label: 'Internal Name', align: 'center', searchable: true },
+    { id: 'startDate', label: 'Start Date', align: 'center' },
+    { id: 'endDate', label: 'End Date', align: 'center' },
+    { id: 'domain', label: 'Domain', align: 'center' },
+    { id: 'teamSize', label: 'Team Size', align: 'center' },
+    { id: 'menuItems', label: '', align: 'center' },
+  ];
+
+  const rows: Data[] = projects.map(project => ({
+    id: project.id ?? '',
+    name: project.name ?? '',
+    internalName: project.internal_name ?? '',
+    startDate: project.start_date ?? '',
+    endDate: project.end_date ?? 'Till now',
+    domain: project.domain ?? '',
+    teamSize: project.team_size ?? null,
+    menuItems: [
+      {
+        text: 'View project',
+        onClick: () => navigate(routes.project(project.id)),
+        disabled: false,
+      },
+      {
+        text: 'Delete project',
+        onClick: () => console.log('deleted'),
+        disabled: currentUser?.role !== 'admin',
+      },
+    ],
+  }));
+
+  return <InitialTable columns={columns} rows={rows} />;
+}
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BreadcrumbsNav } from '../components';
+import ProjectsTable from '../components/tables/ProjectsTable';
 
 export default function Projects() {
   return (
     <>
       <BreadcrumbsNav paths={[{ text: 'Projects' }]} />
+      <ProjectsTable />
     </>
   );
 }
