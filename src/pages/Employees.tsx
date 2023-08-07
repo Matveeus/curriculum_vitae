cbf83c34222ac5a6577d7e6f9db9e7ea7b99bcd9
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-08-07 11:30:27 +0300

    move data fetching logic up the component tree

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,42 +1,15 @@
-import React, { useEffect } from 'react';
-import { useLazyQuery } from '@apollo/client';
-import { GET_USERS } from '../apollo/operations';
+import React from 'react';
 import { useTypedSelector } from '../hooks/useTypedSelector';
-import { useTypedDispatch } from '../hooks/useTypedDispatch';
-import { usersSelectors, setUsers } from '../store/usersSlice';
-import { BreadcrumbsNav, EmployeesTable, Loader, ErrorBar } from '../components';
-import type { User } from '../apollo/types';
-
-interface QueryResult {
-  users: User[];
-}
+import { usersSelectors } from '../store/usersSlice';
+import { BreadcrumbsNav, EmployeesTable } from '../components';
 
 export default function Employees() {
-  const dispatch = useTypedDispatch();
   const users = useTypedSelector(usersSelectors.selectAll);
-  const [getUsers, { loading, error, data }] = useLazyQuery<QueryResult>(GET_USERS);
-
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  useEffect(() => {
-    if (data) {
-      dispatch(setUsers(data.users));
-    }
-  }, [data]);
-
-  const dataExist = users.length > 0;
-
-  if (loading && !dataExist) {
-    return <Loader />;
-  }
 
   return (
     <>
       <BreadcrumbsNav paths={[{ text: 'Employees' }]} />
       <EmployeesTable users={users} />
-      {error && !dataExist ? <ErrorBar error={error.message} /> : null}
     </>
   );
 }
