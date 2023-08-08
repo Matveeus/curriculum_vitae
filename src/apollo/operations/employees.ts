d511d64ca97beebe6b9a4b0ed3a49295f070c5f9
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-08-08 18:46:58 +0300

    refactor update profile form

--- src/apollo/operations/employees.ts
+++ src/apollo/operations/employees.ts
@@ -1,59 +1,65 @@
 import { gql } from '@apollo/client';
 
+const USER_DATA = gql`
+  fragment UserData on User {
+    department_name
+    position_name
+    department {
+      id
+    }
+    position {
+      id
+    }
+  }
+`;
+
+const PROFILE_DATA = gql`
+  fragment ProfileData on Profile {
+    id
+    first_name
+    last_name
+    full_name
+    avatar
+    skills {
+      skill_name
+      mastery
+    }
+    languages {
+      language_name
+      proficiency
+    }
+  }
+`;
+
 export const GET_USERS = gql`
+  ${USER_DATA}
+  ${PROFILE_DATA}
   query GetUsers {
     users {
+      ...UserData
       id
       created_at
       email
-      department_name
-      position_name
       role
-      department {
-        id
-      }
-      position {
-        id
-      }
       cvs {
         id
         name
       }
       profile {
-        id
-        first_name
-        last_name
-        full_name
-        avatar
-        skills {
-          skill_name
-          mastery
-        }
-        languages {
-          language_name
-          proficiency
-        }
+        ...ProfileData
       }
     }
   }
 `;
 
 export const UPDATE_USER = gql`
+  ${USER_DATA}
+  ${PROFILE_DATA}
   mutation UpdateUser($id: ID!, $user: UpdateUserInput!) {
     updateUser(id: $id, user: $user) {
-      id
-      department_name
-      position_name
+      ...UserData
       profile {
-        first_name
-        last_name
-        full_name
-      }
-      department {
-        id
-      }
-      position {
-        id
+        ...ProfileData
       }
     }
   }
