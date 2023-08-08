86a2e8495a66ba31ac8e624837004cd77b2ab288
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-08-08 13:44:26 +0300

    remove unused query

--- src/apollo/operations/employees.ts
+++ src/apollo/operations/employees.ts
@@ -1,65 +1,38 @@
 import { gql } from '@apollo/client';
 
-const USER_DATA = gql`
-  fragment UserData on User {
-    id
-    created_at
-    email
-    department_name
-    position_name
-    role
-    department {
-      id
-    }
-    position {
-      id
-    }
-    cvs {
-      id
-      name
-    }
-  }
-`;
-
-const PROFILE_DATA = gql`
-  fragment ProfileData on Profile {
-    id
-    first_name
-    last_name
-    full_name
-    avatar
-    skills {
-      skill_name
-      mastery
-    }
-    languages {
-      language_name
-      proficiency
-    }
-  }
-`;
-
 export const GET_USERS = gql`
-  ${USER_DATA}
-  ${PROFILE_DATA}
   query GetUsers {
     users {
-      ...UserData
-      profile {
-        ...ProfileData
+      id
+      created_at
+      email
+      department_name
+      position_name
+      role
+      department {
+        id
       }
-    }
-  }
-`;
-
-export const GET_USER = gql`
-  ${USER_DATA}
-  ${PROFILE_DATA}
-  query GetUser($id: ID!) {
-    user(id: $id) {
-      ...UserData
+      position {
+        id
+      }
+      cvs {
+        id
+        name
+      }
       profile {
-        ...ProfileData
+        id
+        first_name
+        last_name
+        full_name
+        avatar
+        skills {
+          skill_name
+          mastery
+        }
+        languages {
+          language_name
+          proficiency
+        }
       }
     }
   }
--- src/apollo/operations/index.ts
+++ src/apollo/operations/index.ts
@@ -1,7 +1,7 @@
 import { SIGN_UP, LOG_IN } from './auth';
-import { GET_USERS, GET_USER, UPDATE_USER } from './employees';
+import { GET_USERS, UPDATE_USER } from './employees';
 import { GET_SELECT_LISTS } from './selectLists';
 import { GET_PROJECTS } from './projects';
 import { GET_CVS } from './cvs';
 
-export { SIGN_UP, LOG_IN, GET_USERS, GET_USER, UPDATE_USER, GET_SELECT_LISTS, GET_PROJECTS, GET_CVS };
+export { SIGN_UP, LOG_IN, GET_USERS, UPDATE_USER, GET_SELECT_LISTS, GET_PROJECTS, GET_CVS };
