02036585c22c1d06c67a9bd3adf292e10b2591dd
Matvey Lapitchev <matvey.lapitchev@gmail.com>
2023-08-10 16:26:12 +0200

    add project selector to get project if there is one

--- src/pages/ProjectDetails.tsx
+++ src/pages/ProjectDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FolderIcon from '@mui/icons-material/FolderCopyOutlined';
 import { BreadcrumbsNav, Loader } from '../components';
 import routes from '../constants/routes';
@@ -7,11 +7,22 @@ import { useQuery } from '@apollo/client';
 import { GET_PROJECT } from '../apollo/operations';
 import InfoBar from '../components/InfoBar';
 import ProjectDetailsForm from '../components/forms/Projects/ProjectDetailsForm';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { addProject, projectsSelectors } from '../store/projectsSlice';
+import { useTypedDispatch } from '../hooks/useTypedDispatch';
 
 export default function ProjectDetails() {
+  const dispatch = useTypedDispatch();
   const { id } = useParams();
-  const { data, error, loading } = useQuery(GET_PROJECT, { variables: { id } });
-  const project = data?.project;
+  const storedProject = useTypedSelector(state => projectsSelectors.selectById(state, id as string));
+  const { data, error, loading } = useQuery(GET_PROJECT, { variables: { id }, skip: !!storedProject });
+  const project = storedProject || data?.project;
+
+  useEffect(() => {
+    if (data) {
+      dispatch(addProject(data?.project));
+    }
+  }, [data]);
 
   if (loading) {
     return <Loader />;
