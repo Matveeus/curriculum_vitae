3320e13a521a91391eb2e4bb735cf1e46874edc4
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-08-15 10:17:01 +0300

    add modal window for creating/updating a user

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,42 +1,57 @@
 import React, { useEffect } from 'react';
-import { useLazyQuery } from '@apollo/client';
-import { GET_USERS } from '../apollo/operations';
+import { useQuery, useLazyQuery } from '@apollo/client';
+import { GET_USERS, GET_SELECT_LISTS } from '../apollo/operations';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useTypedDispatch } from '../hooks/useTypedDispatch';
 import { usersSelectors, setUsers } from '../store/usersSlice';
+import Box from '@mui/material/Box';
 import { BreadcrumbsNav, EmployeesTable, Loader, InfoBar } from '../components';
-import type { User } from '../apollo/types';
+import type { User, Department, Position } from '../apollo/types';
 
-interface QueryResult {
+interface UsersQueryResult {
   users: User[];
+}
+
+interface ListsQueryResult {
+  departments: Department[];
+  positions: Position[];
 }
 
 export default function Employees() {
   const dispatch = useTypedDispatch();
   const users = useTypedSelector(usersSelectors.selectAll);
-  const [getUsers, { loading, error, data }] = useLazyQuery<QueryResult>(GET_USERS);
+  const [getUsers, usersQuery] = useLazyQuery<UsersQueryResult>(GET_USERS);
+  const listsQuery = useQuery<ListsQueryResult>(GET_SELECT_LISTS);
 
   useEffect(() => {
     getUsers();
   }, []);
 
   useEffect(() => {
-    if (data) {
-      dispatch(setUsers(data.users));
+    if (usersQuery.data) {
+      dispatch(setUsers(usersQuery.data.users));
     }
-  }, [data]);
+  }, [usersQuery.data]);
 
-  const dataExist = users.length > 0;
+  const loading = usersQuery.loading || listsQuery.loading;
+  const error = usersQuery.error || listsQuery.error;
 
-  if (loading && !dataExist) {
+  const departments = listsQuery.data?.departments ?? [];
+  const positions = listsQuery.data?.positions ?? [];
+
+  if (loading) {
     return <Loader />;
   }
 
   return (
     <>
-      <BreadcrumbsNav paths={[{ text: 'Employees' }]} />
-      <EmployeesTable users={users} />
-      {error && !dataExist ? <InfoBar text={error.message} status="error" /> : null}
+      <Box sx={{ mb: 2 }}>
+        <BreadcrumbsNav paths={[{ text: 'Employees' }]} />
+      </Box>
+
+      <EmployeesTable departments={departments} positions={positions} users={users} />
+
+      {error ? <InfoBar text={error.message} status="error" /> : null}
     </>
   );
 }
