1ce4126bc98f815a0259248f7d48b68cd5d1afe6
Matvey Lapitchev <matvey.lapitchev@gmail.com>
2023-08-17 00:40:09 +0200

    add ability to add and remove language/skill

--- src/components/forms/CVs/CvDetailsFrom.tsx
+++ src/components/forms/CVs/CvDetailsFrom.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { Cv, Language, Skill } from '../../../apollo/types';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { Controller, SubmitHandler, useForm, useFieldArray } from 'react-hook-form';
 import Button from '@mui/material/Button';
 import { useQuery } from '@apollo/client';
 import { GET_CV_LISTS } from '../../../apollo/operations';
@@ -12,6 +12,7 @@ import { InputValues } from './cvFormInterfaces';
 import Typography from '@mui/material/Typography';
 import InfoBar from '../../InfoBar';
 import Divider from '@mui/material/Divider';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 interface CvFormProps {
   cv: Cv;
@@ -39,19 +40,66 @@ export default function CvDetailsForm({ cv }: CvFormProps) {
   const {
     control,
     handleSubmit,
-    formState: { isDirty },
+    formState: { isDirty, errors },
   } = useForm({
     defaultValues: initialValues,
   });
 
+  const {
+    remove: removeLanguage,
+    append: appendLanguage,
+    fields: languageFields,
+  } = useFieldArray({
+    control,
+    name: 'languages',
+  });
+
+  const {
+    remove: removeSkill,
+    append: appendSkill,
+    fields: skillFields,
+  } = useFieldArray({
+    control,
+    name: 'skills',
+  });
+
+  const handleAddLanguage = () => {
+    appendLanguage({ language_name: '', proficiency: '' });
+  };
+
+  const handleAddSkill = () => {
+    appendSkill({ skill_name: '', mastery: '' });
+  };
+
+  const handleRemoveLanguage = (languageIndex: number) => {
+    removeLanguage(languageIndex);
+  };
+
+  const handleRemoveSkill = (skillIndex: number) => {
+    removeSkill(skillIndex);
+  };
+
   const renderLanguageFields = () => {
-    return initialValues.languages.map((language, index) => (
-      <Box key={language.language_name} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', mt: 3 }}>
+    return languageFields.map((language, index) => (
+      <Box key={language.id} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', mt: 3 }}>
+        <Button onClick={() => handleRemoveLanguage(index)} variant="outlined">
+          <DeleteForeverIcon />
+        </Button>
         <Controller
           name={`languages.${index}.language_name`}
           control={control}
+          defaultValue={language.language_name}
+          rules={{ required: 'Language is required' }}
           render={({ field }) => (
-            <TextField sx={{ width: '70%' }} select label="Language" fullWidth {...field}>
+            <TextField
+              sx={{ width: '55%' }}
+              select
+              label="Language"
+              fullWidth
+              {...field}
+              error={!!errors.languages?.[index]?.language_name}
+              helperText={errors.languages?.[index]?.language_name?.message}
+            >
               {allLanguages.map(({ id, name }) => (
                 <MenuItem value={name} key={id}>
                   {name}
@@ -63,8 +111,18 @@ export default function CvDetailsForm({ cv }: CvFormProps) {
         <Controller
           name={`languages.${index}.proficiency`}
           control={control}
+          defaultValue={language.proficiency}
+          rules={{ required: 'Proficiency is required' }}
           render={({ field }) => (
-            <TextField sx={{ width: '27%' }} select label="Proficiency" fullWidth {...field}>
+            <TextField
+              sx={{ width: '23%' }}
+              select
+              label="Proficiency"
+              fullWidth
+              {...field}
+              error={!!errors.languages?.[index]?.proficiency}
+              helperText={errors.languages?.[index]?.proficiency?.message}
+            >
               {proficienciesList.map(proficiency => (
                 <MenuItem value={proficiency} key={proficiency}>
                   {proficiency}
@@ -78,13 +136,26 @@ export default function CvDetailsForm({ cv }: CvFormProps) {
   };
 
   const renderSkillFields = () => {
-    return initialValues.languages.map((language, index) => (
-      <Box key={language.language_name} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', mt: 3 }}>
+    return skillFields.map((skill, index) => (
+      <Box key={skill.id} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', mt: 3 }}>
+        <Button onClick={() => handleRemoveSkill(index)} variant="outlined">
+          <DeleteForeverIcon />
+        </Button>
         <Controller
           name={`skills.${index}.skill_name`}
           control={control}
+          defaultValue={skill.skill_name}
+          rules={{ required: 'Skill is required' }}
           render={({ field }) => (
-            <TextField sx={{ width: '70%' }} select label="Skill" fullWidth {...field}>
+            <TextField
+              sx={{ width: '55%' }}
+              select
+              label="Skill"
+              fullWidth
+              {...field}
+              error={!!errors.skills?.[index]?.skill_name}
+              helperText={errors.skills?.[index]?.skill_name?.message}
+            >
               {allSkills.map(({ id, name }) => (
                 <MenuItem value={name} key={id}>
                   {name}
@@ -96,8 +167,18 @@ export default function CvDetailsForm({ cv }: CvFormProps) {
         <Controller
           name={`skills.${index}.mastery`}
           control={control}
+          defaultValue={skill.mastery}
+          rules={{ required: 'Mastery is required' }}
           render={({ field }) => (
-            <TextField sx={{ width: '27%' }} select label="Mastery" fullWidth {...field}>
+            <TextField
+              sx={{ width: '23%' }}
+              select
+              label="Mastery"
+              fullWidth
+              {...field}
+              error={!!errors.skills?.[index]?.mastery}
+              helperText={errors.skills?.[index]?.mastery?.message}
+            >
               {masteryList.map(mastery => (
                 <MenuItem value={mastery} key={mastery}>
                   {mastery}
@@ -147,11 +228,17 @@ export default function CvDetailsForm({ cv }: CvFormProps) {
         <Box sx={{ width: '32%' }}>
           <Typography variant="h6">Languages</Typography>
           {renderLanguageFields()}
+          <Button variant="outlined" onClick={handleAddLanguage} fullWidth sx={{ mt: 3 }}>
+            Add Language
+          </Button>
         </Box>
         <Divider orientation="vertical" flexItem />
         <Box sx={{ width: '32%' }}>
           <Typography variant="h6">Skills</Typography>
           {renderSkillFields()}
+          <Button variant="outlined" onClick={handleAddSkill} fullWidth sx={{ mt: 3 }}>
+            Add Skill
+          </Button>
         </Box>
       </Box>
       {error ? <InfoBar text={error.message} status="error" /> : null}
